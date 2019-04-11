8860fabfbb269d02a9fd201a82a012c75c395686
fankys2102 <fankys2012@gmail.com>
2019-04-11 19:11:00 +0800

    register

--- chatroom/server/model/userDao.go
+++ chatroom/server/model/userDao.go
@@ -1,7 +1,8 @@
 package model
 
 import (
-	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"github.com/garyburd/redigo/redis"
 )
@@ -22,34 +23,77 @@ func NewUserDao(pool *redis.Pool) (userDao *UserDao) {
 	}
 }
 func (this *UserDao) getUserById(conn redis.Conn, id int) (user *User, err error) {
-	res, err := redis.String(conn.Do("HGet", "users", id))
+	key := "user:" + strconv.Itoa(id)
+	value, err := redis.StringMap(conn.Do("hgetall", key))
 	if err != nil {
 		//在redis 中没有获取到值
 		if err == redis.ErrNil {
-
+			err = ERROR_USER_NOTEXISTS
+			return
 		}
 		return
 	}
+	user = &user
+	//string -> int
+	uid, err := strconv.Atoi(value["id"])
+	fmt.Printf("%T vale = %d", uid, uid)
+	// if err == nil {
+	user.UserId = uid
+	// }
 
-	err = json.Unmarshal([]byte(res), &user)
-	if err != nil {
-		return
-	}
+	// user.UserName = value["name"]
+	// user.UserPwd = value["pwd"]
+	// fmt.Println(value)
 	return
 }
 
 func (this *UserDao) Login(id int, pwd string) (user *User, err error) {
 	conn := this.pool.Get()
-
 	defer conn.Close()
 
 	user, err = this.getUserById(conn, id)
 	if err != nil {
 		return
 	}
+	fmt.Println(user)
 
-	if user.UserPwd != pwd {
+	// if user.UserPwd != pwd {
+	// 	return
+	// }
+	return
+}
+
+//通过ID判断用户是否存在
+//false 不存在 ；true 存在
+func (this *UserDao) ExistsById(id int) (bool, error) {
+	conn := this.pool.Get()
+	defer conn.Close()
+	key := "user:" + strconv.Itoa(id)
+	_, err := redis.Int(conn.Do("hget", key, "id"))
+	if err != nil {
+		//在redis 中没有获取到值
+		if err == redis.ErrNil {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (this *UserDao) Register(user *User) (err error) {
+
+	//用户ID是否存在
+	exists, err := this.ExistsById(user.UserId)
+	if err != nil {
+		return
+	} else if exists {
+		err = ERROR_USER_EXISTS
 		return
 	}
+	conn := this.pool.Get()
+	defer conn.Close()
+	key := "user:" + strconv.Itoa(user.UserId)
+	_, err = conn.Do("hmset", key, "id", user.UserId, "name", user.UserName, "pwd", user.UserPwd)
 	return
+
 }
